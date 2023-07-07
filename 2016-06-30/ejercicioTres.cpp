34a0841ee763f8a037484d6ce8cb444bfa6adc40
Merok23 <alejocalizblanco@gmail.com>
2023-07-07 13:22:14 -0300

    Se agrego el ejercicio 3 del final, no lo pienso ni correr.

--- 2016-06-30/ejercicioTres.cpp
+++ 2016-06-30/ejercicioTres.cpp
@@ -7,6 +7,13 @@ renglón). El servidor debe cerrarse al recibir un paquete de 8 bytes 00h.
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
+#include <stdio.h>
+//'\0' == 0000 = 0 (para mi)
+bool ended(char* buffer) {
+    for (int i = 0; i < 8; i++) if (buffer[i] != '\0') return false;
+    return true;
+}
+
 int main(int argc, char** argv) {
     //Suponiendo que me pasan un puerto por argv:
     if (argc != 2) return;
@@ -22,6 +29,18 @@ int main(int argc, char** argv) {
     int file_descriptor = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     //bindeamos el socket para aceptar conecciones:
     error = bind(file_descriptor, result->ai_addr, result->ai_addrlen);
-
+    if (error == -1) return;
+    error = listen(file_descriptor, 5);
+    int recieving_socket = accept(file_descriptor, nullptr, nullptr);
+    bool finished = false;
+    char buffer[9];
+    buffer[8] = '\0';
+    size_t recieved;
+    while (!finished) {
+        recieved = 0;
+        while(recieved < 8) recieved = recv(recieving_socket, buffer + recieved, 8, 0);
+        if (ended(buffer)) finished = true;
+        printf("Recieved: %s", buffer);
+    }
     return 0;
 }
